4fc1a3c502b46d9c3ae6705abb6582d5f291038a
NaveenKumar3026 <naveen3026mit@gmail.com>
2026-05-29 22:56:56 +0530

    first commit

---
+++ myscript.js
@@ -0,0 +1,37 @@
+function showCourses(){
+    fetch("http://localhost:8080/courses") // API ENDPOINT
+    .then((response) => response.json())
+    .then((courses) => {
+        const dataTable = document.getElementById("coursetable");
+
+        courses.forEach(course => {
+            var row = `<tr>
+                <td>${course.courseId}</td>
+                <td>${course.courseName}</td>
+                <td>${course.trainer}</td>
+                <td>${course.durationInWeeks}</td>
+            </tr>`
+
+            dataTable.innerHTML += row;
+        });
+    });
+}
+
+function showEnrolledStudents(){
+    fetch("http://localhost:8080/courses/enrolled") // API ENDPOINT
+    .then((response) => response.json())
+    .then((students) => {
+        const dataTable = document.getElementById("enrolledtable");
+
+        students.forEach(student => {
+            var row = `<tr>
+                <td>${student.id}</td>
+                <td>${student.name}</td>
+                <td>${student.emailId}</td>
+                <td>${student.courseName}</td>
+            </tr>`
+
+            dataTable.innerHTML += row;
+        });
+    });
+}
